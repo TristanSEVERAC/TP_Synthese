55c283e5b2030c1c085a0fb7edc5d7f144557cb2
unknown <p1201753@IUTDOUAB2PC3.univ-lyon1.fr>
2016-01-06 15:31:43 +0100

    Fin Question 1

--- password.py
+++ password.py
@@ -9,20 +9,21 @@ def getNext(password):
     >>> getNext('bc')
     'bd'
     """
-    pwd = list(password)  #1
+    pwd = list(password)  #1 pwd est une variable qui récupère la valeur en entrée de password transformé en List: (abc) devient ['a', 'b', 'c']
     found = False
     i=len(pwd)-1
-
     while not found:
         if pwd[i] < 'z':
-           pwd[i] = chr(ord(pwd[i])+1)  #2
+           pwd[i] = chr(ord(pwd[i])+1)  #2 incrémente de 1 la valeur ascii du dernier caractère contenu dans pwd
            found = True             
         else:
-           i = i-1 
+           pwd[i] = chr(ord(pwd[i])- 25)
+           i = i - 1
+           if pwd[i] < 'z':
+               pwd[i] = chr(ord(pwd[i])+1)
+               found = True
     
-    return ''.join(pwd) #3
-
-
+    return ''.join(pwd) #3 convertie toutes les valeurs de la variable pwd en une seule chaine: a,b,c devient abc
 
 # Grâce à ce fragment de code, si vous exécutez ce fichier, les tests doctests seront exécutés également. 
 # Si vous ne voulez plus que les tests s'exécutent, commentez les deux lignes ci-dessous. 
